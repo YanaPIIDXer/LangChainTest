eeb76940749d8971e0591c89e23ff71b4a19072d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-01 22:42:28 +0900

    PromptTemplateのテスト

--- src/EntryPoint.ts
+++ src/EntryPoint.ts
@@ -1,11 +1,20 @@
 import { OpenAI } from "langchain/llms/openai";
+import { PromptTemplate } from "langchain/prompts";
 
 export default async () => {
   const chatBot = new OpenAI({
     temperature: 0.9,
   });
-  const response = await chatBot.call(
-    "TypeScriptでHello Worldを書いてください。また、ソースコードも解説してください。"
-  );
-  console.log(response);
+  const promptTemplate = new PromptTemplate({
+    inputVariables: ["lang"],
+    template: "{lang}でHello Worldを書いてください",
+  });
+
+  const langs = ["TypeScript", "C#", "C++", "Python", "PHP"];
+  for (const lang of langs) {
+    console.log("==== " + lang + " ====");
+    const prompt = await promptTemplate.format({ lang });
+    const response = await chatBot.call(prompt);
+    console.log(response + "\n");
+  }
 };
