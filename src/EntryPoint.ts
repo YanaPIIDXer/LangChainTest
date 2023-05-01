1beb7d9bf3d8d69a100448cd2bd52b5efbd80725
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-02 06:13:03 +0900

    天気を取得するAPIを叩くToolを実装 既にあるみたいだけど実験なので気にしない

--- src/EntryPoint.ts
+++ src/EntryPoint.ts
@@ -1,27 +1,25 @@
 import { OpenAI } from "langchain/llms/openai";
 import * as Tools from "langchain/tools";
 import * as Agents from "langchain/agents";
-import { parse as parseYaml } from "yaml";
-import * as fs from "fs";
-import { fetchWeather } from "./Weather";
+import { WeatherFetchTool } from "./Weather";
 
 export default async () => {
-  // const chatBot = new OpenAI({
-  //   modelName: "gpt-3.5-turbo",
-  //   temperature: 0.2,
-  // });
-  // // ↓ChatGPTに生成してもらったTODOアプリのAPI仕様
-  // //  一応管理対象外とする
-  // const yamlOrigin = fs
-  //   .readFileSync("./secrets/openapi/TodoManage.yaml")
-  //   .toString();
-  // const yaml = parseYaml(yamlOrigin);
-  // const jsonSpec = new Tools.JsonSpec(yaml);
-  // const jsonToolkit = new Agents.JsonToolkit(jsonSpec);
-  // const jsonAgent = Agents.createJsonAgent(chatBot, jsonToolkit);
-  // const response = await jsonAgent.call({
-  //   input: "Todoを作るには？日本語で答えてください。",
-  // });
-  // console.log(response.output);
-  await fetchWeather("東京都");
+  const chatBot = new OpenAI({
+    modelName: "gpt-3.5-turbo",
+    temperature: 0.2,
+  });
+
+  const agent = await Agents.initializeAgentExecutorWithOptions(
+    [new WeatherFetchTool()],
+    chatBot,
+    {
+      //verbose: true,
+      agentType: "zero-shot-react-description",
+    }
+  );
+
+  const response = await agent.call({
+    input: "東京都の天気を教えてください。出力は日本語でお願いします。",
+  });
+  console.log(response);
 };
--- src/Weather.ts
+++ src/Weather.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
+import { Tool } from "langchain/tools";
 
-export const fetchWeather = async (place: string): Promise<void> => {
+/**
+ * 指定した場所の天気を取得する
+ * @param place 場所
+ */
+export const fetchWeather = async (place: string): Promise<string> => {
   try {
     const response = await axios.get(
       "https://api.openweathermap.org/data/2.5/weather",
@@ -16,8 +21,21 @@ export const fetchWeather = async (place: string): Promise<void> => {
     if (response.status !== 200) {
       throw response;
     }
-    console.log(response.data);
+    return response.data.weather[0].main;
   } catch (error) {
     console.error(error);
+    throw error;
   }
 };
+
+/**
+ * 天気予報取得Tool
+ * 車輪の再発明になってるっぽいけど気にしない
+ */
+export class WeatherFetchTool extends Tool {
+  protected async _call(arg: any): Promise<string> {
+    return await fetchWeather(arg);
+  }
+  name: string = "Weather Fetch Tool";
+  description: string = "API to get weather forecast";
+}
