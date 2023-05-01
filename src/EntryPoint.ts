335d55f455f53879e08702b1e894f326462b57c5
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-02 04:32:11 +0900

    ChatGPTに生成してもらったTodoアプリのAPI仕様（OpenAPI形式のファイル。一応非公開）を渡して、それを元にTodoの作り方を聞いてみる処理を実装

--- src/EntryPoint.ts
+++ src/EntryPoint.ts
@@ -1,35 +1,26 @@
 import { OpenAI } from "langchain/llms/openai";
-import {
-  SystemMessagePromptTemplate,
-  HumanMessagePromptTemplate,
-  ChatPromptTemplate,
-} from "langchain/prompts";
-import { ConversationChain } from "langchain/chains";
-import { ConversationSummaryMemory } from "langchain/memory";
+import * as Tools from "langchain/tools";
+import * as Agents from "langchain/agents";
+import { parse as parseYaml } from "yaml";
 import * as fs from "fs";
 
 export default async () => {
-  // ↓「魔法少女まどか☆マギカ」の「鹿目まどか」になりきらせるためのプロンプト
-  //   アニメ上での台詞をサンプルに含むので、テキストファイルはリポジトリの管理外としている
-  const madokaPrompt = fs
-    .readFileSync("./templates/secrets/Madoka.txt")
-    .toString();
-
   const chatBot = new OpenAI({
-    modelName: "gpt-3.5-turbo", // MEMO: こいつを記述したら何故か空文字問題が解決した
+    modelName: "gpt-3.5-turbo",
     temperature: 0.2,
   });
 
-  const prompt = ChatPromptTemplate.fromPromptMessages([
-    SystemMessagePromptTemplate.fromTemplate(madokaPrompt),
-    HumanMessagePromptTemplate.fromTemplate("{text}"),
-  ]);
-
-  const chain = new ConversationChain({
-    llm: chatBot,
-    prompt,
+  // ↓ChatGPTに生成してもらったTODOアプリのAPI仕様
+  //  一応管理対象外とする
+  const yamlOrigin = fs
+    .readFileSync("./secrets/openapi/TodoManage.yaml")
+    .toString();
+  const yaml = parseYaml(yamlOrigin);
+  const jsonSpec = new Tools.JsonSpec(yaml);
+  const jsonToolkit = new Agents.JsonToolkit(jsonSpec);
+  const jsonAgent = Agents.createJsonAgent(chatBot, jsonToolkit);
+  const response = await jsonAgent.call({
+    input: "Todoを作るには？日本語で答えてください。",
   });
-
-  const response = await chain.predict({ text: "あなたの名前は？" });
-  console.log(response || "No Message"); // MEMO: 何故か空文字が返ってきていた
+  console.log(response.output);
 };
