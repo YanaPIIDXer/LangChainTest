8172a23f2358269e14e6cb30f178581ad436af58
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-02 12:57:43 +0900

    フロントエンド雛型

--- python/main.py
+++ python/main.py
@@ -1,1 +1,12 @@
-print("Hello")
+# 参考: https://qiita.com/tkmrsksk/items/7362f183138dfb324c50
+
+import gradio as gr
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox()
+    clear = gr.Button("Clear")
+
+    if __name__ == "__main__":
+        print("Launch...")
+        demo.launch(inbrowser=True)
